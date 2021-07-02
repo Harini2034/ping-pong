6c1525bc5342609dfbe67b88a49461b5589c14fe
Alexandria <72262425+Harini2034@users.noreply.github.com>
2021-07-02 18:59:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,13 +23,24 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
 
-function draw(){
-
- background(0); 
-
+function draw()
+{
+ image(video, 0, 0, 700, 600);
+ background(0);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
